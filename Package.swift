e0dedc36cfbb5bff96183f7df10aab3e9fd9b844
Long Vu <long.vudai01@gmail.com>
2025-09-18 16:28:09 +0700

    Update swift-algorithms package version to 1.2.1

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .package(path: "../AnalyticsKit"),
         .package(path: "../CommonKitUI"),
         .package(path: "../FoundationX"),
-        .package(url: "https://github.com/apple/swift-algorithms", exact: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.1"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.1"),
         .package(url: "https://github.com/CombineCommunity/CombineExt", from: "1.8.1")
     ],
